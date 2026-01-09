91f627c3c87bb11d40b4cdf17b0a748bee726e97
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-09 10:43:54 -0300

    first commit

---
+++ src/main/java/io/github/aplaraujo/DesafioRitsTecnologiaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRitsTecnologiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRitsTecnologiaBackendApplication.class, args);
+	}
+
+}
