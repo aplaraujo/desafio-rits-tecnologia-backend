51d520f3527be56977f7a7c4a9e8858cd4b29ea4
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-12 08:36:35 -0300

    Product entity

---
+++ src/main/java/io/github/aplaraujo/entities/Product.java
@@ -0,0 +1,20 @@
+package io.github.aplaraujo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
