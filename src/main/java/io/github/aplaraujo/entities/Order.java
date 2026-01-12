3d511e9e863b2836151f2f9db9282023f0cc1165
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-12 15:38:22 -0300

    Many to many relationship - order x product refactored

--- src/main/java/io/github/aplaraujo/entities/Order.java
+++ src/main/java/io/github/aplaraujo/entities/Order.java
@@ -2,13 +2,21 @@ package io.github.aplaraujo.entities;
 
 import io.github.aplaraujo.entities.enums.OrderStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
-public class Order {
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Order extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,6 +28,10 @@ public class Order {
     private Client client;
 
     @ManyToMany
-    @JoinTable(name = "tb_order_product", joinColumns = @JoinColumn(name = "product_id"),  inverseJoinColumns = @JoinColumn(name = "order_id"))
+    @JoinTable(
+            name = "tb_order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
     private Set<Product> products = new HashSet<>();
 }
