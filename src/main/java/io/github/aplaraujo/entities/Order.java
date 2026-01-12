e77ad9cfb86c7c72b31b09a8034c45ea334bc401
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-12 09:00:51 -0300

    Order entity

---
+++ src/main/java/io/github/aplaraujo/entities/Order.java
@@ -0,0 +1,16 @@
+package io.github.aplaraujo.entities;
+
+import io.github.aplaraujo.entities.enums.OrderStatus;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long clientId;
+    private Long productId;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+}
