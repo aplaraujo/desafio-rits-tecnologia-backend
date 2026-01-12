8a4715d3b9aaa637dd48339618f3b75cfa630d2b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-12 09:33:49 -0300

    Database seeding with clients

--- src/main/java/io/github/aplaraujo/entities/Client.java
+++ src/main/java/io/github/aplaraujo/entities/Client.java
@@ -17,6 +17,7 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String email;
     private String phone;
     private String address;
     private String password;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,9 @@
+INSERT INTO tb_client(name, email, phone, address, password) VALUES('Caroline Nair da Costa', 'carolinenairdacosta@outllok.com', '(53) 99678-1222', 'Rua Alexandre Mendonça, 668', 'qo7w7BN7wX');
+INSERT INTO tb_client(name, email, phone, address, password) VALUES('Hugo Vitor Martins', 'hugovitormartins@iclud.com', '(96) 98749-2210', 'Rua F, 370', 'AgZ463qGli');
+INSERT INTO tb_client(name, email, phone, address, password) VALUES('Giovanni Henrique Cavalcanti', 'giovannihenriquecavalcanti@megamega.com.br', '(34) 99206-8731', 'Rua Garcia, 865', 'x4bysnwYYh');
+INSERT INTO tb_client(name, email, phone, address, password) VALUES('Rafael Miguel Bernardes', 'rafael_miguel_bernardes@cosma.com', '(68) 98547-5574', 'Travessa Tefé, 411', '1dst13mohf');
+INSERT INTO tb_client(name, email, phone, address, password) VALUES('Isis Sophia Dias', 'isis_dias@iclud.com', '(27) 98128-4005', 'Rua João Cipreste Filho, 354', 'BDPJO7Q269');
+
 INSERT INTO tb_product(name, price) VALUES ('X-Burger', 12.0);
 INSERT INTO tb_product(name, price) VALUES ('X-Salada', 14.0);
 INSERT INTO tb_product(name, price) VALUES ('X-Bacon', 16.0);
