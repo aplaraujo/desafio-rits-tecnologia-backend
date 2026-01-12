21898aa4900f8c56e0ba087f291111f2a78030ee
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-12 08:54:11 -0300

    Base entity for order auditing

---
+++ src/main/java/io/github/aplaraujo/entities/BaseEntity.java
@@ -0,0 +1,15 @@
+package io.github.aplaraujo.entities;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    private LocalDateTime createdAt;
+}
