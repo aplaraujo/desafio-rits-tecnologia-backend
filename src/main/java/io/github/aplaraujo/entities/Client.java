476add43957369da153552ca64bc0f76b118949a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-13 17:26:55 -0300

    Relationship client and role and UserDetailsImpl

--- src/main/java/io/github/aplaraujo/entities/Client.java
+++ src/main/java/io/github/aplaraujo/entities/Client.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_client")
@@ -27,4 +29,11 @@ public class Client {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_client_role", joinColumns = @JoinColumn(name = "client_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    @Transient
+    private List<String> authority;
 }
