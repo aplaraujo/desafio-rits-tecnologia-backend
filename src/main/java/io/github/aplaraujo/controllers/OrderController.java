bfe8f619e089bbb5a09a0f81e953c694fcd78714
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-14 20:18:23 -0300

    Search orders

--- src/main/java/io/github/aplaraujo/controllers/OrderController.java
+++ src/main/java/io/github/aplaraujo/controllers/OrderController.java
@@ -1,4 +1,33 @@
 package io.github.aplaraujo.controllers;
 
+import io.github.aplaraujo.dto.OrderDTO;
+import io.github.aplaraujo.entities.Order;
+import io.github.aplaraujo.mappers.OrderMapper;
+import io.github.aplaraujo.security.UserDetailsImpl;
+import io.github.aplaraujo.services.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService service;
+    private final OrderMapper mapper;
+
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @GetMapping
+    public ResponseEntity<List<OrderDTO>> findOrders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long clientId = userDetails.getId();
+        List<OrderDTO> orders = service.searchAllByClient(clientId);
+        return ResponseEntity.ok(orders);
+    }
 }
--- src/main/java/io/github/aplaraujo/services/OrderService.java
+++ src/main/java/io/github/aplaraujo/services/OrderService.java
@@ -1,7 +1,9 @@
 package io.github.aplaraujo.services;
 
+import io.github.aplaraujo.dto.OrderDTO;
 import io.github.aplaraujo.entities.Order;
 import io.github.aplaraujo.entities.Product;
+import io.github.aplaraujo.mappers.OrderMapper;
 import io.github.aplaraujo.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,14 +12,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository repository;
+    private final OrderMapper mapper;
 
     public Page<Order> search(Product product, Integer page, Integer pageSize) {
         Pageable pageable = PageRequest.of(page, pageSize);
         return repository.findAllWithProducts(pageable);
     }
+
+    public List<OrderDTO> searchAllByClient(Long clientId) {
+        List<Order> orders = clientId != null ? repository.findByClientId(clientId) : repository.findAll();
+        return orders.stream().map(mapper::toDTO).collect(Collectors.toList());
+    }
+
 }
